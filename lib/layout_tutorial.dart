a639f38342f95936426adb61654a29a6dbf07a69
suli <sss908938889@163.com>
2019-03-16 20:48:14 +0800

    Adding interactivity

--- lib/layout_tutorial.dart
+++ lib/layout_tutorial.dart
@@ -81,7 +81,7 @@ class LayoutTutorialWidget extends StatelessWidget {
             textSelection,
             Container(
               margin: EdgeInsets.all(16),
-              child: TabboxA(),
+              child: ParentWidget(),
             )
         ],
         ),
@@ -193,3 +193,127 @@ class _TabboxAState extends State<TabboxA> {
 
 }
 
+
+//-------------------------------- ParentWidget --------------------------//
+
+class ParentWidget extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _ParentWidgetState();
+
+}
+
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTabboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TabboxC(active: _active, onChanged: _handleTabboxChanged),
+    );
+  }
+}
+
+//-------------------------------- TabboxB --------------------------//
+
+class TabboxB extends StatelessWidget {
+  const TabboxB({Key key, this.active, this.onChanged}) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            color: active ? Colors.lightGreen[700] : Colors.grey[700]
+        ),
+      ),
+    );
+  }
+
+}
+
+
+class TabboxC extends StatefulWidget {
+  TabboxC({Key key, this.active, this.onChanged}) : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  State<StatefulWidget> createState() => _TabboxCState();
+}
+
+class _TabboxCState extends State<TabboxC> {
+  bool _highlight = false;
+
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        child: Center(
+          child: Text(
+            widget.active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(
+            color: widget.active ? Colors.lightGreen[700] : Colors.grey[700],
+            border: _highlight ? Border.all(color: Colors.teal[700], width: 10.0) : null
+        ),
+      ),
+    );
+  }
+
+}
